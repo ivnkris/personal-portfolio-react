ef78d529a7048ca071e485abcb37e78cb05592f5
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 13:18:05 +0100

    styled resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,6 @@
 const Resume = (props) => {
   return (
-    <div>
+    <div class="element-container">
       <a href="./assets/pdf/CV_Krisztian_Ivan.pdf" download>
         Download my Resume
       </a>
