82b353de824c5c7f7bdebd77b263ed82b9c2cd42
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 11:12:56 +0100

    Added Project components to Portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -20,6 +20,46 @@ const Portfolio = (props) => {
               title="P1xel"
               description="A digital gaming library with a social element"
             />
+            <Project
+              repoLink="https://github.com/ivnkris/geo-link"
+              imgLink="./assets/images/geo-link.png"
+              imgAlt="Geo Link - a platform to re-connect with your city"
+              deployedLink="http://krisztianivan.com"
+              title="Geo Link"
+              description="A platform to re-connect with your city"
+            />
+            <Project
+              repoLink="https://github.com/ivnkris/employee-management-cms-tool"
+              imgLink="./assets/images/employee-management-cms.png"
+              imgAlt="Employee Management CMS Tool"
+              deployedLink="https://github.com/ivnkris/employee-management-cms-tool"
+              title="Employee Management CMS"
+              description="Command line workforce management application"
+            />
+            <Project
+              repoLink="https://github.com/ivnkris/team-profile-generator"
+              imgLink="./assets/images/team-profile-generator.png"
+              imgAlt="Team Profile Generator"
+              deployedLink="https://github.com/ivnkris/team-profile-generator"
+              title="Team Profile Generator"
+              description="Command line organisational chart generator"
+            />
+            <Project
+              repoLink="https://github.com/ivnkris/weather-dashboard"
+              imgLink="./assets/images/weather-dashboard.png"
+              imgAlt="weather dashboard screenshot"
+              deployedLink="https://ivnkris.github.io/weather-dashboard/"
+              title="Weather Dashboard"
+              description="Check the weather in your local area"
+            />
+            <Project
+              repoLink="https://github.com/ivnkris/code-quiz"
+              imgLink="./assets/images/code-quiz.png"
+              imgAlt="code quiz screenshot"
+              deployedLink="https://ivnkris.github.io/code-quiz/"
+              title="Code Quiz"
+              description="Test your JavaScript knowledge"
+            />
           </section>
         </div>
       </main>
