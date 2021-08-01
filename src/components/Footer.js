fd0fc8d084801d526e3b4bb43207f499b46085a0
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 11:02:59 +0100

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = (props) => {
+  return (
+    <footer>
+      <div class="footer--content">
+        Coded by: Krisztian Ivan | Coding bootcamp graduation date: August 2021
+        | Available to hire!
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,23 +1,29 @@
 import Project from "../components/Project";
+import Footer from "../components/Footer";
 
 const Portfolio = (props) => {
   return (
-    <div class="element-container">
-      <div class="heading--container">
-        <h2 id="personal-portfolio" class="animation-1">
-          Personal Portfolio
-        </h2>
-      </div>
-      <section class="portfolio--container">
-        <Project
-          repoLink="https://github.com/ivnkris/p1xel"
-          imgLink="./assets/images/p1xel.png"
-          imgAlt="P1xel - A digital gaming library with a social element"
-          deployedLink="https://fast-shelf-77951.herokuapp.com"
-          title="P1xel"
-          description="A digital gaming library with a social element"
-        />
-      </section>
+    <div>
+      <main>
+        <div class="element-container">
+          <div class="heading--container">
+            <h2 id="personal-portfolio" class="animation-1">
+              Personal Portfolio
+            </h2>
+          </div>
+          <section class="portfolio--container">
+            <Project
+              repoLink="https://github.com/ivnkris/p1xel"
+              imgLink="./assets/images/p1xel.png"
+              imgAlt="P1xel - A digital gaming library with a social element"
+              deployedLink="https://fast-shelf-77951.herokuapp.com"
+              title="P1xel"
+              description="A digital gaming library with a social element"
+            />
+          </section>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
