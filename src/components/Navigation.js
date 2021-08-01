bf933910fb6ec1bb16cc92c3ec32ba70f9360c8b
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 12:52:49 +0100

    remved public url form navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,9 @@
 const Navigation = (props) => {
   return (
     <nav>
-      <a href="%PUBLIC_URL%/">About Me</a>
-      <a href="%PUBLIC_URL%/portfolio">Portfolio</a>
-      <a href="%PUBLIC_URL%/contact">Contact</a>
+      <a href="/">About Me</a>
+      <a href="/portfolio">Portfolio</a>
+      <a href="/contact">Contact</a>
       <a href="./assets/pdf/CV_Krisztian_Ivan.pdf" download>
         Resume
       </a>
