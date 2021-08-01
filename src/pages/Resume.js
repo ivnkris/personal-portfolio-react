ab2ae54ad0acbd49430bb6746bc81f217ccc7495
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 14:50:37 +0100

    added skills to resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,34 @@
+import Footer from "../components/Footer";
+
 const Resume = (props) => {
   return (
-    <div class="element-container">
-      <a href="./assets/pdf/CV_Krisztian_Ivan.pdf" download>
-        Download my Resume
-      </a>
+    <div>
+      <main>
+        <div>
+          <h2>Known Technologies:</h2>
+          <section>
+            <ul>
+              <li>
+                <strong>Languages:</strong> HTML, CSS, JavaScript, React,
+                jQuery, Node.js, JSON, Express, Web-APIs, Third-party APIs,
+                RESTful APIs, Moment.js, Bootstrap, Handlebars.js, Git, mySQL,
+                Sequelize, MongoDB, Mongoose and GraphQL
+              </li>
+              <li>
+                <strong>Applications:</strong> MS Visual Studio Code, NPM,
+                Postman, GitHub, Heroku, mySQL Workbench and Robo 3T
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <div class="element-container">
+          <a href="./assets/pdf/CV_Krisztian_Ivan.pdf" download>
+            Download my Resume
+          </a>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
