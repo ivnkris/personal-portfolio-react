6342744ea84604c69e30804dcf74784d3d4d44a2
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-01 12:05:53 +0100

    added contact links into footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,32 @@
 const Footer = (props) => {
   return (
-    <footer>
-      <div class="footer--content">
-        Coded by: Krisztian Ivan | Coding bootcamp graduation date: August 2021
-        | Available to hire!
+    <footer class="footer--content">
+      <div class="contact-me--item">
+        <i class="fab fa-linkedin"></i>
+        <a
+          href="https://www.linkedin.com/in/krisztian-ivan-10880478/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linkedin
+        </a>
+      </div>
+
+      <div class="contact-me--item">
+        <i class="fab fa-github"></i>
+        <a href="https://github.com/ivnkris" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      </div>
+
+      <div class="contact-me--item">
+        <a
+          href="mailto:krisztian.ivan@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          E-mail
+        </a>
       </div>
     </footer>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,40 +12,6 @@ const Contact = (props) => {
           </div>
 
           <div class="right-border"></div>
-
-          <section class="contact-me--container">
-            <div class="contact-me--item animation-7">
-              <i class="fab fa-linkedin"></i>
-              <a
-                href="https://www.linkedin.com/in/krisztian-ivan-10880478/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Linkedin
-              </a>
-            </div>
-
-            <div class="contact-me--item animation-8">
-              <i class="fab fa-github"></i>
-              <a
-                href="https://github.com/ivnkris"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>
-            </div>
-
-            <div class="contact-me--item">
-              <a
-                href="mailto:krisztian.ivan@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                E-mail
-              </a>
-            </div>
-          </section>
         </div>
       </main>
       <Footer />
